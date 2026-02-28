5959db12f22ab5219fa2192b2d5442345c51b6d1
subham2001 <50857233+subham2001@users.noreply.github.com>
2026-02-28 13:37:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const users = []; // store 2 sockets
+let users = [];
 
 app.use(express.static("public"));
 
 io.on("connection", socket => {
-  // Limit to 2 users
+  // Limit room to 2 people
   if (users.length >= 2) {
     socket.emit("room-full");
     socket.disconnect();
@@ -20,21 +20,16 @@ io.on("connection", socket => {
 
   users.push(socket);
 
-  // When both present, notify
-  if (users.length === 2) {
-    users.forEach(s => s.emit("peer-ready"));
-  }
-
-  // Relay signals directly
-  socket.on("signal", data => {
-    const other = users.find(s => s !== socket);
-    if (other) other.emit("signal", data);
+  // Relay messages directly
+  socket.on("message", msg => {
+    socket.broadcast.emit("message", msg);
   });
 
   socket.on("disconnect", () => {
-    const i = users.indexOf(socket);
-    if (i !== -1) users.splice(i, 1);
+    users = users.filter(u => u !== socket);
   });
 });
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000, () =>
+  console.log("Server running")
+);
